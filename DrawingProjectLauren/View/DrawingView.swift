71004149a33e8f2a7ff6ce1e742587d28539f975
Lauren <davislau2001@gmail.com>
2018-11-01 12:29:03 -0600

    Created DrawingView cocoa touch file in view folder.

---
+++ DrawingProjectLauren/View/DrawingView.swift
@@ -0,0 +1,24 @@
+//
+//  DrawingView.swift
+//  DrawingProjectLauren
+//
+//  Created by Davis, Lauren on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class DrawingView: UIView
+{
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    public override func draw(_ rect: CGRect) -> Void
+     {
+        // Drawing code
+        
+    }
+    
+
+}
