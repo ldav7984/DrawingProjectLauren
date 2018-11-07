2dcb7108c40a29134eca6f828e5624d0ade55818
Davis <ldav7984@740s-pl-109.local>
2018-11-07 13:34:20 -0700

    Connected IBOutlets in the ArtCell view file.

---
+++ DrawingProjectLauren/View/ArtCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCell.swift
+//  DrawingProjectLauren
+//
+//  Created by Davis, Lauren on 11/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var artImage: UIImageView!
+    @IBOutlet weak var artLabel: UILabel!
+    
+}
