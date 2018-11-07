437cde6e2cd66f9488855eed595b33429436a996
Davis <ldav7984@740s-pl-109.local>
2018-11-07 12:49:53 -0700

    Fixed typo so the lists were one-to-one

--- DrawingProjectLauren/Controller/ArtCollectionViewController.swift
+++ DrawingProjectLauren/Controller/ArtCollectionViewController.swift
@@ -70,20 +70,20 @@ public class ArtCollectionViewController: UICollectionViewController
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int
+    public override func numberOfSections(in collectionView: UICollectionView) -> Int
     {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of items
         return 0
     }
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
